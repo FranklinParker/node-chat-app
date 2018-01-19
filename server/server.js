10261a3eaff10f9a670b13f6871d674a93bc6196
Franklin Parker <fparker@deliveryman.com>
2018-01-18 21:18:46 -0500

    socket

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
-var app = express();
 
+const port = process.env.PORT || 3000;
+
+var app = express();
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+const server = http.createServer(app);
+
+var io = socketIO(server);
+
+io.on('connection',(socket)=>{
+   console.log('new user connected');
+    socket.on('disconnect', () => {
+        console.log('Client disconnected from server' );
+    });
+});
+
+
+server.listen(port, () => {
     console.log(`Server is up on ${port}`);
 });
