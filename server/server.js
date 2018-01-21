60c9c2208ea66a87ff04d2c0a7963c3263c0e2b4
Franklin Parker <fparker@deliveryman.com>
2018-01-20 22:30:04 -0500

    chat app

--- server/server.js
+++ server/server.js
@@ -15,22 +15,28 @@ const server = http.createServer(app);
 
 var io = socketIO(server);
 io.on('connection', (socket) => {
-    console.log('New user connected');
+  console.log('New user connected');
 
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-    socket.on('createMessage', (message, callback) => {
-        console.log('createMessage', message);
-        io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('This is from the server.');
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server.');
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+  });
 
-    });
-    socket.on('disconnect', () => {
-        console.log('User was disconnected');
-    });
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
 });
+
 
 server.listen(port, () => {
     console.log(`Server is up on ${port}`);
